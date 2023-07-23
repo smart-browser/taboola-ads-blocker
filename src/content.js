35293f8ad2534586aa2dd6cd1da196573d0c1286
Uriel Zohar <urielfed@gmail.com>
2023-07-23 13:56:13 +0300

    mute consoles log

--- src/content.js
+++ src/content.js
@@ -3,10 +3,8 @@ const REMOVE_PERIOD_INTERVAL = 1000 * 10;
 const GET_SELECTORS = 'GET_SELECTORS';
 
 (async () => {
-    console.log('Selectors message started');
     const selectors = await chrome.runtime.sendMessage({message: GET_SELECTORS}) || [];
     const ELEMENT_SELECTORS_TO_REMOVE = [...selectors];
-    console.log('ELEMENT_SELECTORS_TO_REMOVE: ', ELEMENT_SELECTORS_TO_REMOVE);
     // do something with response here, not outside the function
     setInterval(() => {
         ELEMENT_SELECTORS_TO_REMOVE.forEach((selector) => {
